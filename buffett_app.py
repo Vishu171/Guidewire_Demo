f1badf4f4d94cab87e0855e928dbd373cf9922a2
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-10-10 19:57:13 +0530

    Update buffett_app.py

--- buffett_app.py
+++ buffett_app.py
@@ -36,7 +36,7 @@ def plot_financials(df_2, x, y, x_cutoff, title):
         y=y
         ).properties(title=title)
     ) 
-  """"    
+  """  
     df_subset = df_2.head(x_cutoff)
     # Create a bar chart using st.bar_chart()
     return st.bar_chart(df_subset.set_index(y)[x])
