c548d16f10673420618bee7a670d7f54c7a441b2
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-10-10 18:12:16 +0530

    Update buffett_app.py

--- buffett_app.py
+++ buffett_app.py
@@ -164,6 +164,7 @@ if authenticate_user():
                             title_name = df_2.columns[0]+'-'+df_2.columns[1]
                             with col2:
                              plot_financials(df_2,df_2.columns[0],df_2.columns[1], cutoff,title_name)
+                             st.write(df_2)
                       #st.session_state.messages.append({"role": "assistant", "content": tabulate(df_2, tablefmt="html",headers=headers,showindex=False)})
                         st.session_state.messages.append({"role": "assistant", "content": df_2.to_csv(sep=',', index=False)})
                         
