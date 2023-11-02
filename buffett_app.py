3c2bcffb87805478d92752e867d74adb86f9966b
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-11-02 12:41:26 +0530

    Update buffett_app.py

--- buffett_app.py
+++ buffett_app.py
@@ -92,7 +92,7 @@ def authenticate_user():
 if authenticate_user():
     with st.sidebar:
       image = Image.open("assets/jadefingeniusnewlogo.png")
-      image = st.image('assets/jadefingeniusnewlogo.png',width=280)
+      image = st.image('assets/jadefingeniusnewlogo.png',width=290)
       selected = option_menu( menu_title="Explore",
       menu_icon = "search",
       options=["Company Statements", 'Annual Reports'], 
