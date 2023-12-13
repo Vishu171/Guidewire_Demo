0640a41c00cd47b18a26017ec0352f9845c2f1a7
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-12-13 11:44:17 +0530

    Update letter_load.py

--- load/letter_load.py
+++ load/letter_load.py
@@ -5,22 +5,12 @@ from langchain.vectorstores import  Pinecone # for the vectorization part
 from langchain.text_splitter import TokenTextSplitter
 import pinecone
 
-#import magic
-#import nltk
-#nltk.download('punkt')
-
 # identify the various pdf files
-pdfs = [file for file in os.listdir('./letters/') if 'pdf' in file]
+pdfs = [file for file in os.listdir('./Guidewireletters/') if 'pdf' in file]
 
-# loops through each pdf in the letters directory
-# and loops the content using langchains PyPDFLoader
-# there can likely be a better way for loading each individual doc
-# but I ran into issues with some of the other loader dependencies
-# ideally each letter would be serialized itself
-# but this approach just consolidates and loads them all in a flat list
 page_list = []
 for pdf in pdfs:
-    pdf_path = f"./letters/{pdf}"
+    pdf_path = f"./Guidewireletters/{pdf}"
     loader = PyPDFLoader(pdf_path)
     pages = loader.load()
     page_list.append(pages)
@@ -30,10 +20,10 @@ flat_list = [item for sublist in page_list for item in sublist]
 # initialize pinecone
 import streamlit as st
 pinecone.init(
-    api_key=st.secrets['pinecone_key'], 
-    environment=st.secrets['pinecone_env'] 
+    api_key=st.secrets['pinecone_key'],
+    environment=st.secrets['pinecone_env']
 )
-index_name = "buffett"
+index_name = "gwrefinance"
 
 text_splitter = TokenTextSplitter(chunk_size=1000, chunk_overlap=0)
 texts = text_splitter.split_documents(flat_list)
